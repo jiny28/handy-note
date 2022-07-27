50e6dd0331e39986815e2c67a46a90d747151cc6
jiny <chenjinyang6@gmail.com>
2022-07-27 17:58:56 +0800

    add mysql and emqx by golang

---
+++ golang/demo/device-access/mqttUtil/mqtt.go
@@ -0,0 +1,74 @@
+package mqttUtil
+
+import (
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"log"
+	"os"
+	"time"
+)
+
+//操作堵塞时间. s
+var actionTimeout int = 1
+
+type MqttConnection struct {
+	Host               []string
+	Client             string
+	Username           string
+	Password           string
+	AutomaticReconnect bool
+	CleanSession       bool
+	connClient         mqtt.Client
+}
+
+func (m *MqttConnection) Connection(f mqtt.MessageHandler) {
+	mqtt.DEBUG = log.New(os.Stdout, "", 0)
+	mqtt.ERROR = log.New(os.Stdout, "", 0)
+	opts := mqtt.NewClientOptions()
+	for _, v := range m.Host {
+		opts.AddBroker(v)
+	}
+	opts.SetClientID(m.Client)
+	opts.SetUsername(m.Username)
+	opts.SetPassword(m.Password)
+	opts.SetAutoReconnect(m.AutomaticReconnect)
+	opts.SetCleanSession(m.CleanSession)
+	opts.SetKeepAlive(60 * time.Second)
+	opts.SetPingTimeout(1 * time.Second)
+	// 设置消息回调处理函数
+	opts.SetDefaultPublishHandler(f)
+	m.connClient = mqtt.NewClient(opts)
+	if token := m.connClient.Connect(); token.WaitTimeout(time.Duration(actionTimeout)*time.Second) && token.Error() != nil {
+		panic(token.Error())
+
+	}
+	mqtt.DEBUG.Println("mqtt connection success")
+}
+
+func (m *MqttConnection) Subscribe(topics map[string]byte, callback mqtt.MessageHandler) error {
+	token := m.connClient.SubscribeMultiple(topics, callback)
+	if token.WaitTimeout(time.Duration(actionTimeout)*time.Second) && token.Error() != nil {
+		return token.Error()
+	}
+	mqtt.DEBUG.Println("subscribe topics:", topics)
+	return nil
+}
+
+func (m *MqttConnection) UnSubscribe(topics ...string) error {
+	if token := m.connClient.Unsubscribe(topics...); token.WaitTimeout(time.Duration(actionTimeout)*time.Second) && token.Error() != nil {
+		return token.Error()
+	}
+	return nil
+}
+
+func (m *MqttConnection) PublishMsg(topic string, qos byte, retain bool, payload interface{}) error {
+	if token := m.connClient.Publish(topic, qos, retain, payload); token.Error() != nil {
+		return token.Error()
+	}
+	mqtt.DEBUG.Println("publish msg on topic:", topic)
+	return nil
+}
+
+func (m *MqttConnection) Disconnection(u uint) {
+	m.connClient.Disconnect(u)
+	mqtt.DEBUG.Println("disconnection.")
+}
---
+++ golang/demo/device-access/mysqlUtil/mysql.go
@@ -0,0 +1,152 @@
+package mysqlUtil
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"strconv"
+	"strings"
+)
+
+type MysqlInfo struct {
+	UserName string
+	Password string
+	Ip       string
+	Port     int
+	Db       string
+}
+
+var MyDB *sql.DB
+
+func Connection(info MysqlInfo) (*sql.DB, error) {
+	path := strings.Join([]string{info.UserName, ":", info.Password, "@tcp(", info.Ip, ":", strconv.Itoa(info.Port), ")/", info.Db, "?charset=utf8"}, "")
+	var err error
+	MyDB, err = sql.Open("mysql", path)
+	if err != nil {
+		return nil, err
+	}
+	MyDB.SetConnMaxLifetime(100)
+	MyDB.SetMaxOpenConns(100)
+	MyDB.SetMaxIdleConns(10)
+	if error := MyDB.Ping(); error != nil {
+		return nil, error
+	}
+	return MyDB, nil
+}
+
+//Add new record to table
+func Add(insertSql string, args ...interface{}) (int64, error) {
+	stmt, err := MyDB.Prepare(insertSql)
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(args...)
+	if err != nil {
+		return 0, err
+	}
+	return result.LastInsertId()
+}
+
+//Exe commands
+//del or update
+func Exe(exeSql string, args ...interface{}) (int64, error) {
+	stmt, err := MyDB.Prepare(exeSql)
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(args...)
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+//GetFirst
+//get one record from query result
+func GetFirst(query string, args ...interface{}) (map[string]string, error) {
+	if !strings.Contains(strings.ToUpper(query), "LIMIT") {
+		query += " LIMIT 1"
+	}
+	stmt, err := MyDB.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query(args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	ret := make(map[string]string, len(scanArgs))
+
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			break
+		}
+		var value string
+		for i, col := range values {
+			if col == nil {
+				value = "" //or NULL
+			} else {
+				value = string(col)
+			}
+			ret[columns[i]] = value
+		}
+		break //get the first row only
+	}
+	return ret, err
+}
+
+//GetAll
+//all records from query result
+func GetAll(query string, args ...interface{}) ([]map[string]string, error) {
+	stmt, err := MyDB.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query(args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	ret := make([]map[string]string, 0)
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			break
+		}
+		var value string
+		vmap := make(map[string]string, len(scanArgs))
+		for i, col := range values {
+			if col == nil {
+				value = "" // or NULL
+			} else {
+				value = string(col)
+			}
+			vmap[columns[i]] = value
+		}
+		ret = append(ret, vmap)
+	}
+	return ret, err
+}

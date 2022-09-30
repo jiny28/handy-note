6fe0989d0a1bf5f565e82dceb2f65878ffca39e4
jiny <chenjinyang6@gmail.com>
2022-09-30 11:45:56 +0800

    complete.

---
+++ golang/dewesoftCsv/main.go
@@ -0,0 +1,257 @@
+package main
+
+import (
+	"bufio"
+	"dewesoftCsv/taosUtil"
+	"encoding/csv"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+var taosInfo = taosUtil.TaosInfo{
+	HostName:   "taos-server",
+	ServerPort: 6030,
+	User:       "root",
+	Password:   "taosdata",
+	DbName:     "hdb",
+}
+var wg sync.WaitGroup
+
+func init() {
+	taosUtil.Connection(taosInfo)
+}
+func main() {
+	rootDir := "D:/csv/"
+	files, _ := ioutil.ReadDir(rootDir)
+	execFiles := make([]string, 0)
+	for _, f := range files {
+		if strings.Contains(f.Name(), ".csv") {
+			s := strings.Split(f.Name(), "-")[1]
+			if len(s) != 13 {
+				fmt.Println(f.Name() + "文件开始时间无效")
+				return
+			}
+			execFiles = append(execFiles, rootDir+f.Name())
+		}
+	}
+	if len(execFiles) == 0 {
+		fmt.Println("无文件可读")
+		return
+	}
+	fmt.Printf("读取文件列表：%v\n", execFiles)
+	for _, v := range execFiles {
+		sinceStart := time.Now()
+		stat, err := os.Stat(v)
+		checkErr(err, "file stat error")
+		start := strings.Split(stat.Name(), "-")[1]
+		startTime, err := strconv.ParseInt(start, 10, 64)
+		checkErr(err, "开始时间格式不正确")
+		csvFile, err := os.Open(v)
+		checkErr(err, "file open error")
+		reader := csv.NewReader(bufio.NewReaderSize(csvFile, 1024*1024*100))
+		lines, err := reader.ReadAll()
+		checkErr(err, "read error")
+		indexRecode := reader.FieldsPerRecord
+		csvFile.Close()
+		if indexRecode == 3 {
+			writeTaosVo(lines, startTime)
+		} else if indexRecode == 13 {
+			writeTaosCur(lines, startTime)
+		}
+		fmt.Printf("总耗时：%v\n", time.Since(sinceStart))
+	}
+
+}
+
+func writeTaosCur(lines [][]string, startTime int64) {
+	startTime = startTime * 1000 //to us
+	var result = make([]taosUtil.SubTableValue, 0)
+	oneInsertSize := 1000
+	rowValues := make([]taosUtil.RowValue, 0)
+	for i := range lines {
+		if i == 0 {
+			continue
+		}
+		line := lines[i]
+		ts, v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11 := line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12]
+		tsFloat, _ := strconv.ParseFloat(ts, 64)
+		tsFloat = tsFloat * 1e6
+		exeTs := int64(tsFloat) + startTime
+		fieldValues := []taosUtil.FieldValue{
+			{
+				Name:  "ts",
+				Value: exeTs,
+			}, {
+				Name:  "value0",
+				Value: v0,
+			}, {
+				Name:  "value1",
+				Value: v1,
+			}, {
+				Name:  "value2",
+				Value: v2,
+			}, {
+				Name:  "value3",
+				Value: v3,
+			}, {
+				Name:  "value4",
+				Value: v4,
+			}, {
+				Name:  "value5",
+				Value: v5,
+			}, {
+				Name:  "value6",
+				Value: v6,
+			}, {
+				Name:  "value7",
+				Value: v7,
+			}, {
+				Name:  "value8",
+				Value: v8,
+			}, {
+				Name:  "value9",
+				Value: v9,
+			}, {
+				Name:  "value10",
+				Value: v10,
+			}, {
+				Name:  "value11",
+				Value: v11,
+			},
+		}
+		rowValues = append(rowValues, taosUtil.RowValue{Fields: fieldValues})
+		if len(rowValues) >= oneInsertSize {
+			var subTableValue taosUtil.SubTableValue
+			subTableValue.Name = "c0"
+			subTableValue.SuperTable = "cur"
+			tags := []taosUtil.TagValue{
+				{
+					Name:  "groupId",
+					Value: "0",
+				},
+			}
+			subTableValue.Tags = tags
+			subTableValue.Values = rowValues
+			result = append(result, subTableValue)
+			rowValues = make([]taosUtil.RowValue, 0)
+		}
+	}
+	fmt.Println(len(result))
+	groupSize := 80 // 每个线程执行40个对象
+	startInsert(groupSize, result)
+}
+
+func writeTaosVo(lines [][]string, startTime int64) {
+	startTime = startTime * 1000 //to us
+	var result = make([]taosUtil.SubTableValue, 0)
+	oneInsertSize := 20000
+	rowValues := make([]taosUtil.RowValue, 0)
+	for i := range lines {
+		if i == 0 {
+			continue
+		}
+		line := lines[i]
+		ts := line[0]
+		v0 := line[1]
+		v1 := line[2]
+		tsFloat, _ := strconv.ParseFloat(ts, 64)
+		tsFloat = tsFloat * 1e6
+		exeTs := int64(tsFloat) + startTime
+		fieldValues := []taosUtil.FieldValue{
+			{
+				Name:  "ts",
+				Value: exeTs,
+			}, {
+				Name:  "value0",
+				Value: v0,
+			}, {
+				Name:  "value1",
+				Value: v1,
+			},
+		}
+		rowValues = append(rowValues, taosUtil.RowValue{Fields: fieldValues})
+		if len(rowValues) >= oneInsertSize {
+			var subTableValue taosUtil.SubTableValue
+			subTableValue.Name = "v0"
+			subTableValue.SuperTable = "vo"
+			tags := []taosUtil.TagValue{
+				{
+					Name:  "groupId",
+					Value: "0",
+				},
+			}
+			subTableValue.Tags = tags
+			subTableValue.Values = rowValues
+			result = append(result, subTableValue)
+			rowValues = make([]taosUtil.RowValue, 0)
+		}
+	}
+	groupSize := 40 // 每个线程执行40个对象
+	startInsert(groupSize, result)
+}
+func insert(array []taosUtil.SubTableValue) {
+	defer wg.Done()
+	for i := range array {
+		_, err := taosUtil.InsertAutoCreateTable(array[i : i+1])
+		checkErr(err, "taos insert error")
+	}
+}
+func startInsert(groupSize int, result []taosUtil.SubTableValue) {
+	max := len(result)
+	var quantity int
+	if max%groupSize == 0 {
+		quantity = max / groupSize
+	} else {
+		quantity = (max / groupSize) + 1
+	}
+	var start, end, i int
+	for i = 1; i <= quantity; i++ {
+		end = i * groupSize
+		if i != quantity {
+			wg.Add(1)
+			go insert(result[start:end])
+		} else {
+			wg.Add(1)
+			go insert(result[start:])
+		}
+		start = i * groupSize
+	}
+	wg.Wait()
+}
+func delFiles(files []string) error {
+	if len(files) == 0 {
+		fmt.Println("需要删除的文件为空")
+		return nil
+	}
+	for _, file := range files {
+		err := os.Remove(file)
+		if err != nil {
+			fmt.Println(file + "文件删除失败:" + err.Error())
+			continue
+		}
+	}
+	return nil
+}
+
+func checkErr(err error, prompt string) {
+	if err != nil {
+		fmt.Printf("error: %s\n", prompt)
+		panic(err.Error())
+	}
+}
+
+/*func test() {
+	start := time.Now()
+	splitter := splitCsv.New()
+	splitter.FileChunkSize = 1024 * 1024 * 500 //in bytes (100MB)
+	result, err := splitter.Split("D:/csv/Test929-1664436811000.csv", "D:/csv/split/")
+	checkErr(err, "")
+	fmt.Println(result)
+	delFiles(result)
+	fmt.Println(time.Since(start))
+}*/
---
+++ golang/dewesoftCsv/taosUtil/taos.go
@@ -0,0 +1,249 @@
+package taosUtil
+
+import (
+	"bytes"
+	"database/sql"
+	"fmt"
+	_ "github.com/taosdata/driver-go/v2/taosSql"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type TaosInfo struct {
+	HostName   string
+	ServerPort int
+	User       string
+	Password   string
+	DbName     string
+}
+
+type SubTableValue struct {
+	SuperTable string
+	Name       string
+	Tags       []TagValue
+	Values     []RowValue
+}
+
+type TagValue struct {
+	Name  string
+	Value interface{}
+}
+
+type RowValue struct {
+	Fields []FieldValue
+}
+
+type FieldValue struct {
+	Name  string
+	Value interface{}
+}
+
+var TaosDb *sql.DB
+var sourceDatabase string
+
+func Connection(info TaosInfo) (*sql.DB, error) {
+	url := "root:taosdata@/tcp(" + info.HostName + ":" + strconv.Itoa(info.ServerPort) + ")/" + info.DbName
+	url += "?charset=utf-8&locale=en_US.UTF-8&timezone=UTC-8&maxSQLLength=1048576"
+	var error error
+	TaosDb, error = sql.Open("taosSql", url)
+	sourceDatabase = info.DbName
+	return TaosDb, error
+}
+
+func Close() {
+	TaosDb.Close()
+}
+
+func InsertAutoCreateTable(subTableValue []SubTableValue) (int64, error) {
+	sqlStr := insertMultiTableMultiValuesUsingSuperTable(subTableValue)
+	res, err := TaosDb.Exec(sqlStr)
+	if err != nil {
+		return -1, err
+	}
+	return res.RowsAffected()
+}
+
+func insertMultiTableMultiValuesUsingSuperTable(subTableValue []SubTableValue) string {
+	head := "insert into "
+	tableArray := make([]string, len(subTableValue))
+	for i, tableValue := range subTableValue {
+		var buffer bytes.Buffer
+		buffer.WriteString(tableValue.Name)
+		buffer.WriteString(" using ")
+		buffer.WriteString(tableValue.SuperTable)
+		buffer.WriteString(" tags ")
+		buffer.WriteString(tagValues(tableValue.Tags))
+		buffer.WriteString(" ")
+		buffer.WriteString(rowFields(tableValue.Values))
+		buffer.WriteString(" values ")
+		buffer.WriteString(rowValues(tableValue.Values))
+		tableArray[i] = buffer.String()
+	}
+	return head + strings.Join(tableArray, " ")
+}
+
+func rowFields(values []RowValue) string {
+	row := values[0].Fields
+	array := make([]string, len(row))
+	for i, value := range row {
+		array[i] = value.Name
+	}
+	return "(" + strings.Join(array, ",") + ")"
+}
+
+func rowValues(values []RowValue) string {
+	tableArray := make([]string, len(values))
+	for i, value := range values {
+		tableArray[i] = fieldValues(value.Fields)
+	}
+	return strings.Join(tableArray, ",")
+}
+
+func fieldValues(fields []FieldValue) string {
+	array := make([]string, len(fields))
+	for i := range array {
+		if i == 0 {
+			array[i] = fmt.Sprintf("%v", fields[i].Value)
+		} else {
+			array[i] = fmt.Sprintf("'%v'", fields[i].Value)
+		}
+	}
+	return "(" + strings.Join(array, ",") + ")"
+}
+
+func tagValues(tags []TagValue) string {
+	tableArray := make([]string, len(tags))
+	for i, tag := range tags {
+		tableArray[i] = fmt.Sprintf("'%v'", tag.Value)
+	}
+	return "(" + strings.Join(tableArray, ",") + ")"
+}
+
+func ExecuteQuery(sqlStr string, database string) ([]map[string]string, error) {
+	if database != "" {
+		err := UseDatabase(database)
+		if err != nil {
+			return nil, err
+		}
+	}
+	rows, err := TaosDb.Query(sqlStr)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	ret := make([]map[string]string, 0)
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			break
+		}
+		var value string
+		vmap := make(map[string]string, len(scanArgs))
+		for i, col := range values {
+			if col == nil {
+				value = "" // or NULL
+			} else {
+				value = string(col)
+			}
+			if columns[i] == "ts" {
+				// 时间转时间戳 2022-08-19T17:56:57+08:00
+				time, _ := time.ParseInLocation(time.RFC3339, value, time.Local)
+				value = strconv.FormatInt(time.UnixNano()/1e6, 10)
+			}
+			vmap[columns[i]] = value
+		}
+		ret = append(ret, vmap)
+	}
+	if database != "" {
+		UseDatabase(sourceDatabase)
+	}
+	return ret, err
+}
+
+func UseDatabase(database string) error {
+	stmt, err := TaosDb.Prepare("use " + database)
+	if err != nil {
+		return err
+	}
+	res, err := stmt.Exec()
+	if err != nil {
+		return err
+	}
+	_, err = res.RowsAffected()
+	return err
+}
+
+func CreateTaosData() []SubTableValue {
+	var subTableValues []SubTableValue
+	startTime := "2022-07-10 10:00:00"
+
+	startDate, _ := time.ParseInLocation("2006-01-02 15:04:05", startTime, time.Local)
+
+	start := startDate.UnixNano() / 1e6
+	// 100个电表，每块电表200个点位
+	for i := 0; i < 1; i++ {
+		var subTableValue SubTableValue
+		is := strconv.Itoa(i)
+		subTableValue.Name = "d00" + is
+		subTableValue.SuperTable = "meters"
+		tags := []TagValue{
+			{
+				Name:  "location",
+				Value: "d00" + is,
+			},
+			{
+				Name:  "groupId",
+				Value: 1 + i,
+			},
+		}
+		subTableValue.Tags = tags
+		num := 100 // 一次插入多少数据
+		rowValues := make([]RowValue, num)
+		for j := 0; j < num; j++ {
+			fieldNum := 200
+			fieldValues := make([]FieldValue, 0)
+			fieldValues = append(fieldValues, FieldValue{
+				Name:  "ts",
+				Value: start,
+			})
+			for a := 0; a < fieldNum; a++ {
+
+				fieldName := "field" + strconv.Itoa(a)
+
+				f := rand.Int() % 1000
+				v := 200 + rand.Float32()
+
+				var fieldValue FieldValue
+				if a%2 == 0 {
+					fieldValue = FieldValue{
+						Name:  fieldName,
+						Value: v,
+					}
+				} else {
+					fieldValue = FieldValue{
+						Name:  fieldName,
+						Value: f,
+					}
+				}
+				fieldValues = append(fieldValues, fieldValue)
+			}
+			rowValues[j] = RowValue{Fields: fieldValues}
+			start += 1000
+		}
+		subTableValue.Values = rowValues
+		subTableValues = append(subTableValues, subTableValue)
+	}
+	return subTableValues
+
+}
